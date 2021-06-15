1598fd1cf120202a71f4274c5704abded69d0009
devanshshukla99 <devanshshukla99@outlook.com>
2021-06-15 22:40:03 +0530

    minor changes

--- pytest_response/interceptors/urllib3_quick.py
+++ pytest_response/interceptors/urllib3_quick.py
@@ -26,13 +26,10 @@ def urlopen_wrapper(func):
             if not data:
                 log.error(f"Response not found url:{_url}")
                 raise ResponseNotFound
-            print(type(headers))
-            log.error(headers)
             return MockResponse(data, headers)
         _ = func(self, method, url, *args, **kwargs)
         if not response.capture:
             return _
-        print(_.headers)
         data = _._fp.read()
         _._fp = io.BytesIO(data)
         response.insert(url=_url, response=data, headers=dict(_.headers))
