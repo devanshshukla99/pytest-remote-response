8e79761bea3095d344a535fdb7e2d823e8e87272
devanshshukla99 <devanshshukla99@outlook.com>
2021-06-23 14:47:54 +0530

    added tests + bug fix

--- pytest_response/interceptors/aiohttp_quick.py
+++ pytest_response/interceptors/aiohttp_quick.py
@@ -1,23 +1,57 @@
-import aiohttp
 from functools import wraps
 
+import aiohttp
+
 from pytest_response import response
-from pytest_response.exceptions import ResponseNotFound
+from pytest_response.app import BaseMockResponse
+from pytest_response.exceptions import RemoteBlockedError, ResponseNotFound
+
+
+class MockResponse(BaseMockResponse):
+    def __init__(self, status, data, headers={}):
+        self.data = data
+        super().__init__(status, b"", headers)
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, *args, **kwargs):
+        pass
+
+    async def text(self):
+        return self.data
+
+    pass
+
+
+def create_wrapper(func):
+    @wraps(func)
+    def inner_func(self, url, *args, **kwargs):
+        if not response.remote:
+            raise RemoteBlockedError
+        if response.response:
+            status, data, headers = response.get(url=str(url))
+            self.status = status
+            if not data:
+                raise ResponseNotFound
+            return MockResponse(200, data, headers)
+        return func(self, url, *args, **kwargs)
+
+    return inner_func
 
 
 def get_wrapper(func):
     @wraps(func)
     async def inner_func(self, *args, **kwargs):
-        if response.response:
-            data, headers = response.get(url=str(self._real_url))
-            if not data:
-                raise ResponseNotFound
-            print(data[:50])
-            return data.decode("utf-8")
+        if not response.remote:
+            raise RemoteBlockedError
         data = await func(self)
         if response.capture:
             response.insert(
-                url=str(self._real_url), response=data.encode("utf-8"), headers=str(self._headers)
+                url=str(self._real_url),
+                response=data.encode("utf-8"),
+                headers=str(dict(self._raw_headers)),
+                status=self.status,
             )
         return data
 
@@ -27,6 +61,11 @@ def get_wrapper(func):
 def install():
     _aiohttpget = aiohttp.ClientResponse.text
     naiohttpget = get_wrapper(_aiohttpget)
+
+    _create_aio = aiohttp.ClientSession.get
+    ncreate_aio = create_wrapper(_create_aio)
+
+    response.mpatch.setattr("aiohttp.ClientSession.get", ncreate_aio)
     response.mpatch.setattr("aiohttp.ClientResponse.text", naiohttpget)
     return
 
