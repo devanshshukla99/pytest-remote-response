c030b2e47698c8fb7ef26b4b2ac5ed3cbc87f84c
devanshshukla99 <devanshshukla99@outlook.com>
2021-06-27 00:41:45 +0530

    simplified tests

--- tests/test_aiohttp.py
+++ tests/test_aiohttp.py
@@ -1,6 +1,9 @@
-def test_remote_block(testdir):
-    testdir.makepyfile(
-        """
+import pytest
+
+
+@pytest.fixture
+def testcode():
+    return """
         import aiohttp
         import pytest
         import asyncio
@@ -38,99 +41,24 @@ def test_remote_block(testdir):
             assert response.db.index() == ["http://www.testingmcafeesites.com/testcat_ac.html",
                                            "https://www.python.org"]
         """
-    )
-    result = testdir.runpytest("-q", "--remote=aiohttp_quick", "--remote-blocked", "-p", "no:warnings")
+
+
+def test_remote_block(testdir, testcode):
+    testdir.makepyfile(testcode)
+    result = testdir.runpytest("-q", "--remote=aiohttp", "--remote-blocked", "-p", "no:warnings")
     result.assert_outcomes(failed=3)
 
 
-def test_remote_connection(testdir):
-    testdir.makepyfile(
-        """
-        import aiohttp
-        import pytest
-        import asyncio
-        from pytest_response import response
-
-        @pytest.fixture(scope="module")
-        def event_loop():
-            loop = loop = asyncio.new_event_loop()
-            yield loop
-            loop.close()
-
-        def test_aiohttp_capture(event_loop):
-            async def async_whats_this():
-                url = "http://www.testingmcafeesites.com/testcat_ac.html"
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(url) as response:
-                        status = response.status
-                        data = await response.text()
-                        assert status == 200
-                        assert data
-            event_loop.run_until_complete(async_whats_this())
-
-        def test_aiohttp_capture_ssl(event_loop):
-            async def async_whats_this():
-                url = "https://www.python.org"
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(url) as response:
-                        status = response.status
-                        data = await response.text()
-                        assert status == 200
-                        assert data
-            event_loop.run_until_complete(async_whats_this())
-
-        def test_database():
-            assert response.db.index() == ["http://www.testingmcafeesites.com/testcat_ac.html",
-                                           "https://www.python.org"]
-        """
-    )
-    result = testdir.runpytest("-q", "--remote=aiohttp_quick", "-p", "no:warnings")
+def test_remote_connection(testdir, testcode):
+    testdir.makepyfile(testcode)
+    result = testdir.runpytest("-q", "--remote=aiohttp", "-p", "no:warnings")
     result.assert_outcomes(passed=2, failed=1)
 
 
-def test_remote_capresp(testdir):
-    testdir.makepyfile(
-        """
-        import aiohttp
-        import pytest
-        import asyncio
-        from pytest_response import response
-
-        @pytest.fixture(scope="module")
-        def event_loop():
-            loop = loop = asyncio.new_event_loop()
-            yield loop
-            loop.close()
-
-        def test_aiohttp_capture(event_loop):
-            async def async_whats_this():
-                url = "http://www.testingmcafeesites.com/testcat_ac.html"
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(url) as response:
-                        status = response.status
-                        data = await response.text()
-                        assert status == 200
-                        assert data
-            event_loop.run_until_complete(async_whats_this())
-
-        def test_aiohttp_capture_ssl(event_loop):
-            async def async_whats_this():
-                url = "https://www.python.org"
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(url) as response:
-                        status = response.status
-                        data = await response.text()
-                        assert status == 200
-                        assert data
-            event_loop.run_until_complete(async_whats_this())
-
-        def test_database():
-            assert response.db.index() == ["http://www.testingmcafeesites.com/testcat_ac.html",
-                                           "https://www.python.org"]
-        """
-    )
-    result = testdir.runpytest("-q", "--remote=aiohttp_quick", "--remote-capture", "-p", "no:warnings")
+def test_remote_capresp(testdir, testcode):
+    testdir.makepyfile(testcode)
+    result = testdir.runpytest("-q", "--remote=aiohttp", "--remote-capture", "-p", "no:warnings")
     result.assert_outcomes(passed=3)
 
-    result = testdir.runpytest("-q", "--remote=aiohttp_quick", "--remote-response", "-p", "no:warnings")
+    result = testdir.runpytest("-q", "--remote=aiohttp", "--remote-response", "-p", "no:warnings")
     result.assert_outcomes(passed=3)
--- tests/test_urllib3.py
+++ tests/test_urllib3.py
@@ -1,6 +1,10 @@
-def test_remote_blocked(testdir):
-    testdir.makepyfile(
-        """
+
+import pytest
+
+
+@pytest.fixture
+def testcode():
+    return """
         import requests
         from pytest_response import response
         def test_urllib3():
@@ -19,124 +23,47 @@ def test_remote_blocked(testdir):
             assert response.db.index() == ["http://www.testingmcafeesites.com/testcat_ac.html",
                                            "https://www.python.org"]
         """
-    )
+
+
+def test_remote_blocked(testdir, testcode):
+    testdir.makepyfile(testcode)
     result = testdir.runpytest("-q", "--remote-blocked", "-p", "no:warnings")
     result.assert_outcomes(failed=3)
 
 
-def test_remote_connection(testdir):
-    testdir.makepyfile(
-        """
-        import requests
-        from pytest_response import response
-        def test_urllib3():
-            url = "http://www.testingmcafeesites.com/testcat_ac.html"
-            res = requests.get(url)
-            assert res.status_code == 200
-            assert res.content
+def test_remote_connection(testdir, testcode):
+    testdir.makepyfile(testcode)
 
-        def test_urllib3_ssl():
-            url = "https://www.python.org"
-            res = requests.get(url)
-            assert res.status_code == 200
-            assert res.content
-
-        def test_database():
-            assert response.db.index() == ["http://www.testingmcafeesites.com/testcat_ac.html",
-                                           "https://www.python.org"]
-        """
-    )
-
-    result = testdir.runpytest("-q", "--remote=urllib_quick|requests_quick", "-p", "no:warnings")
+    result = testdir.runpytest("-q", "--remote=urllib3", "-p", "no:warnings")
     result.assert_outcomes(passed=2, failed=1)
 
 
-def test_remote_capresp(testdir):
-    testdir.makepyfile(
-        """
-        import requests
-        from pytest_response import response
-        def test_urllib3():
-            url = "http://www.testingmcafeesites.com/testcat_ac.html"
-            res = requests.get(url)
-            assert res.status_code == 200
-            assert res.content
-
-        def test_urllib3_ssl():
-            url = "https://www.python.org"
-            res = requests.get(url)
-            assert res.status_code == 200
-            assert res.content
-
-        def test_database():
-            assert response.db.index() == ["http://www.testingmcafeesites.com/testcat_ac.html",
-                                           "https://www.python.org"]
-        """
-    )
+def test_remote_capresp(testdir, testcode):
+    testdir.makepyfile(testcode)
 
     result = testdir.runpytest(
-        "-q", "--remote=urllib_quick|requests_quick", "--remote-capture", "-p", "no:warnings"
+        "-q", "--remote=urllib3", "--remote-capture", "-p", "no:warnings"
     )
     result.assert_outcomes(passed=3)
 
     result = testdir.runpytest(
-        "-q", "--remote=urllib_quick|requests_quick", "--remote-response", "-p", "no:warnings"
+        "-q", "--remote=urllib3", "--remote-response", "-p", "no:warnings"
     )
     result.assert_outcomes(passed=3)
 
 
-def test_remote_connection_full(testdir):
-    testdir.makepyfile(
-        """
-        import requests
-        from pytest_response import response
-        def test_urllib3():
-            url = "http://www.testingmcafeesites.com/testcat_ac.html"
-            res = requests.get(url)
-            assert res.status_code == 200
-            assert res.content
+def test_remote_connection_full(testdir, testcode):
+    testdir.makepyfile(testcode)
 
-        def test_urllib3_ssl():
-            url = "https://www.python.org"
-            res = requests.get(url)
-            assert res.status_code == 200
-            assert res.content
-
-        def test_database():
-            assert response.db.index() == ["http://www.testingmcafeesites.com/testcat_ac.html",
-                                           "https://www.python.org"]
-        """
-    )
-
-    result = testdir.runpytest("-q", "--remote=urllib|urllib3", "-p", "no:warnings")
+    result = testdir.runpytest("-q", "--remote=urllib3_full", "-p", "no:warnings")
     result.assert_outcomes(passed=2, failed=1)
 
 
-def test_remote_capresp_full(testdir):
-    testdir.makepyfile(
-        """
-        import requests
-        from pytest_response import response
-        def test_urllib3():
-            url = "http://www.testingmcafeesites.com/testcat_ac.html"
-            res = requests.get(url)
-            assert res.status_code == 200
-            assert res.content
+def test_remote_capresp_full(testdir, testcode):
+    testdir.makepyfile(testcode)
 
-        def test_urllib3_ssl():
-            url = "https://www.python.org"
-            res = requests.get(url)
-            assert res.status_code == 200
-            assert res.content
-
-        def test_database():
-            assert response.db.index() == ["http://www.testingmcafeesites.com/testcat_ac.html",
-                                           "https://www.python.org"]
-        """
-    )
-
-    result = testdir.runpytest("-q", "--remote=urllib|urllib3", "--remote-capture", "-p", "no:warnings")
+    result = testdir.runpytest("-q", "--remote=urllib3_full", "--remote-capture", "-p", "no:warnings")
     result.assert_outcomes(passed=3)
 
-    result = testdir.runpytest("-q", "--remote=urllib|urllib3", "--remote-response", "-p", "no:warnings")
+    result = testdir.runpytest("-q", "--remote=urllib3_full", "--remote-response", "-p", "no:warnings")
     result.assert_outcomes(passed=3)
