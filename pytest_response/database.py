c60c4c65f112ad3363735c719ea9b4aa1998e5ec
devanshshukla99 <devanshshukla99@outlook.com>
2022-06-04 17:05:52 +0530

    switching to sqlite3 from tinydb

--- pytest_response/database.py
+++ pytest_response/database.py
@@ -1,16 +1,20 @@
 import ast
 import zlib
+import sqlite3
 from base64 import b64decode, b64encode
 from typing import List, Tuple
 from datetime import date
 from urllib.parse import urljoin, urlparse
 
-from tinydb import TinyDB, where
-
 from pytest_response.exceptions import MalformedUrl
 
 __all__ = ["ResponseDB"]
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 class ResponseDB:
     """
@@ -32,11 +36,26 @@ class ResponseDB:
 
     def __init__(self, path: str) -> None:
         self._path = path
-        self._database = TinyDB(path)
+        self._database = sqlite3.connect(path)
+        self.setup()
         return
 
     def __repr__(self) -> str:
         return f"<database {self._path}>"
+    
+    def setup(self) -> bool:
+        self._database.cursor().executescript("""
+            CREATE TABLE IF NOT EXISTS records (
+            url TEXT PRIMARY KEY NOT NULL,
+            cache_date DATE NOT NULL,
+            status TEXT NOT NULL,
+            headers TEXT NOT NULL,
+            response TEXT NOT NULL
+        );
+        """)
+        self._database.commit()
+        self._database.row_factory = dict_factory
+        return True
 
     def index(self, index: str = "url") -> List[str]:
         """
@@ -52,7 +71,7 @@ class ResponseDB:
         _occurances : `list`
             All occurances of the selected column `index`.
         """
-        elements = self._database.all()
+        elements = self._database.execute("SELECT * FROM records;").fetchall()
         _occurances = []
         for element in elements:
             _occurances.append(element.get(index))
@@ -99,7 +118,10 @@ class ResponseDB:
         kwargs.update({"status": str(status)})
         kwargs.update({"headers": b64encode(zlib.compress(str(headers).encode("utf-8"))).decode("utf-8")})
         kwargs.update({"response": b64encode(zlib.compress(response)).decode("utf-8")})
-        self._database.upsert(kwargs, where("url") == url)
+
+        _kwargs = [b64encode(self._sanatize_url(url).encode()).decode(), self.today, str(status), b64encode(zlib.compress(str(headers).encode("utf-8"))).decode("utf-8"), b64encode(zlib.compress(response)).decode("utf-8")]
+        self._database.execute("REPLACE INTO records(url, cache_date, status, headers, response) VALUES (?,?,?,?,?)", _kwargs)
+        self._database.commit()
         return
 
     def get(self, url: str, **kwargs) -> Tuple[int, bytes, dict]:
@@ -121,8 +143,10 @@ class ResponseDB:
         headers : `dict`
             Response header.
         """
-        query = where("url") == self._sanatize_url(url)  # and where("request") == "req"
-        element = self._database.search(query)
+        # query = where("url") == self._sanatize_url(url)  # and where("request") == "req"
+        # element = self._database.search(query)
+        url = b64encode(self._sanatize_url(url).encode()).decode()
+        element = self._database.execute(f"SELECT * FROM records WHERE url='{url}';").fetchall()
         if element:
             status = element[-1].get("status", 200)
             headers = element[-1].get("headers", "[]")
@@ -142,23 +166,27 @@ class ResponseDB:
         -------
         Return list of all elements.
         """
-        return self._database.all()
+        return self._database.execute("SELECT * FROM records").fetchall()
 
     def truncate(self):
         """
         Method to purge all records in the database.
         """
-        return self._database.truncate()
+        self._database.execute("DELETE FROM records;")
+        return self._database.commit()
 
     def close(self) -> None:
-        if isinstance(self._database, TinyDB):
-            if self._database._opened:
-                self._database.close()
+        self._database.close()
+
+        # if isinstance(self._database, TinyDB):
+        #     if self._database._opened:
+        #         self._database.close()
 
     def __del__(self):
-        if isinstance(self._database, TinyDB):
-            if self._database._opened:
-                self._database.close()
+        self._database.close()
+        # if isinstance(self._database, TinyDB):
+        #     if self._database._opened:
+        #         self._database.close()
         return
 
     pass
